247e077ab7434b15dcf9ade427880139db1d88f4
Abhinoor <avhehar@gmail.com>
2021-06-13 09:58:56 -0700

    Added new logic and game is now working. Next time should be a little more careful with my variables

--- RPS Project/rps.py
+++ RPS Project/rps.py
@@ -1,30 +1,41 @@
+#Here we import the random module so we can get the computer to randomly choose between rock, paper, and scissors.
 import random
 
-choice = ("rock", "paper", "scissors")
-computer = (random.choice(choice))
+#Here we set the choices variable equal to a list comprised of rock, paper, and scissors.
+choices = ["rock", "paper", "scissors"]
+#Here a computer varaible is set equal to random.choice which randomly selects one of the three options from the list.
+computer = (random.choice(choices))
 
+#A simple print statement to welcome the user into the game.
 print("Welcome to the Rock, Paper, Scissors Game")
 
-user = input("Your Choice: ").lower()
+'''
+We then define a new userChoice varaible that is equal to the input that a user puts 
+in and we make it run through the code as lower case using the .lower() method.
+'''
+userChoice = input("Your Choice: ").lower()
+#Here we tell the user the random choice the computer made using a print statement.
 print("Computers choice:", computer)
 
-if choice == "rock" and computer == "scissors":
-    print("Rock beats scissors. You win!")
-elif choice == "scissors" and computer == "rock":
-    print("Scissors can't beat rock. You lose!")
-elif choice == "paper" and computer == "rock":
-    print("Paper beats rock. You Win!")
-elif choice == "rock" and computer == "paper":
-    print("Rock can't beat paper. You lose!")
-elif choice == "scissors" and computer == "paper":
-    print("Scissors beats paper. You Win!")
-
-if choice == "rock":
-    #rock
-    #scissors
-    #paper
-    print("Rock beats scissors. You win!")
-elif choice == "scissors":
-    print("Scissors can't beat rock. You lose!")
-elif choice == "paper":
-    print("Paper beats rock. You Win!")
+#Lastly we define the series of possible outcomes and set the winning and losing configurations.
+if userChoice == "rock":
+    if computer == "rock":
+        print("Draw. Rock can't beat rock.")
+    if computer == "scissors":
+        print("Rock beats scissors. You win!")
+    if computer == "paper":
+        print("Rock can't beat paper. You lose :(")
+elif userChoice == "scissors":
+    if computer == "rock":
+        print("Scissors can't beat rock. You lose :(")
+    if computer == "scissors":
+        print("Draw. Scissors can't beat scissors.")
+    if computer == "paper":
+        print("Scissors beats paper. You win!")
+elif userChoice == "paper":
+    if computer == "rock":
+        print("Paper beats rock. You win!")
+    if computer == "scissors":
+        print("Paper can't beat scissors. You lose :(")
+    if computer == "paper":
+        print("Draw. Paper can't beat paper.")
